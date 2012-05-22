6230f3bd6cd0ac48485782518b41fea9e84f0305
Vasiliy Evseenko <vasiliy.evseenko@qik.com>
2012-05-22 11:58:52 +0400

    Fix description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-version = '0.0.1'
+version = '0.0.2'
 
 setup(
     name='hyperloglog',
@@ -11,5 +11,11 @@ setup(
     maintainer_email='svpcom@gmail.com',
     packages=['hyperloglog', 'hyperloglog.test'],
     license='LGPL',
-    long_description=open('README.rst').read(),
+    long_description=\
+"""
+Python implementation of the Hyper LogLog cardinality counter 
+algorithms.
+
+http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.76.4286
+""",
 )
