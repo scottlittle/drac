a9fc98c6b7748560ff69792aa1ac753ef505d385
scott <scott.alan.little@gmail.com>
2021-08-31 16:38:06 -0500

    new version on pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-version = '0.1.3'
+version = '0.1.4'
 
 setup(
     name='drac',
@@ -23,13 +23,21 @@ setup(
         'sortedcontainers>=2.4.0',
     ],
     license='LGPL 2.1 or later',
+    classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'License :: OSI Approved :: LGPL 2.1 or later',
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
     long_description=\
 """
 Python implementation of the HyperLogLog cardinality estimation (counting 1..2..3, hahaha) with support for intersections.
 
 Originally forked from svpcom/hyperloglog. See these references for math background:
 http://algo.inria.fr/flajolet/Publications/FlFuGaMe07.pdf
-http://hal.archives-ouvertes.fr/docs/00/46/53/13/PDF/sliding_HyperLogLog.pdf
 http://research.google.com/pubs/pub40671.html
 """,
 )
